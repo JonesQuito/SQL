cd319af0632f9957e60e443a0c08f8d8e63ef135
JonesQuito <jonesdhy@hotmail.com>
2018-05-30 19:11:45 -0300

    Criando procedures auxiliares e refinando a função exportToFile

--- SQL SERVER/exportToFile.sql
+++ SQL SERVER/exportToFile.sql
@@ -1,47 +1,92 @@
 /*
 	- FUNÇÃO PARA EXPORTAR DADOS DE UMA TABELA DO SQL SERVER PARA UM ARQUIVO .txt
 	- PARA A FUNÇÃO FUNCIONAR CORRETAMENTE É NECESSÁRIO HABILITAR O xp_cmdshell
-	- EM VERSÕES FUTURAS, ESTA FUNÇÃO HABILITARÁ E DESABILITARÁ ESTA FUNCIONALIDADE 
-			EM TEMPO DE EXECUÇÃO
-
+	- PARA EVITAR A NECESSIDADE DE HABILITAR O xp_cmdshell, PODE-SE USAR A 
+		PROCEDURE runExportToFile(@datasource text, @file_target text). A EXECUÇÃO
+		DA REFERIDA PROCEDURE HABILITA O xp_cmdshell, EXECUTA A FUNÇÃO exportToFile E
+		EM SEGIDA DESABILITA O xp_cmdshell
+		
 	@param: datasource (composto pelo nome do banco + nome do eschema + nome da tabela
 	@param: file_target(é o caminho (diretório + nome do arquivo) para onde os dados devem ser exportados
+	@param: uma string com opções de configuração do BCP. O padrão é ' -T -c -t;'
 
 	@return: retorna um integer indicando o total de erros ocorrido durante a execução da função
+
+	EXEMPLOS DE USO: 
+		- select dbo.exportToFile('db_Escola.dbo.tbl_alunos', 'C:/BCP/tbl_alunos4.txt')
+		- select dbo.exportToFile('bd_exemplos_devmedia.dbo.VENDAS','C:/BCP/tblVendas.txt')
 */
-CREATE FUNCTION exportToFile(@datasource text, @file_target text) RETURNS INTEGER AS
+CREATE FUNCTION exportToFile(@datasource text, @file_target text, @options text = ' -T -c -t;') RETURNS INTEGER AS
 BEGIN
 	DECLARE @error INTEGER,
 			@cmd_string varchar(150)
 
-	SET @cmd_string = CONCAT('BCP ', @datasource, ' OUT ', @file_target, ' -T -c -t;')
+	SET @cmd_string = CONCAT('BCP ', @datasource, ' OUT ', @file_target, @options)
 	EXEC @error = master..xp_cmdshell @cmd_string
 	RETURN @error
 END
 go
 
 
-/*
-	EXEMPLOS DE USO DA FUNÇÃO exportToFile(@datasource, @file_target)
-*/
-select dbo.exportToFile('db_Escola.dbo.tbl_alunos', 'C:/BCP/tbl_alunos4.txt')
-select dbo.exportToFile('bd_exemplos_devmedia.dbo.VENDAS','C:/BCP/tblVendas.txt')
-
-
-
-
-
-
 
 
 /*
-	COMO HABILITAR O xp_cmdshell
+	PROCEDURE RESPONSÁVEL POR HABILITAR O xp_cmdshell
+
+	EXEMPLO DE USO:
+		- exec dbo.enable_xp_cmdshell
 */
-exec sp_configure 'show advanced options', 1
+CREATE PROCEDURE enable_xp_cmdshell AS
+BEGIN
+	exec sp_configure 'show advanced options', 1
+	reconfigure
+	EXEC sp_configure 'xp_cmdshell', 1
+	reconfigure	
+	RETURN
+END
 go
-reconfigure
+
+
+
+
+/*
+	PROCEDURE RESPONSÁVEL POR DESABILITAR O xp_cmdshell
+
+	EXEMPLO DE USO:
+		- exec dbo.disable_xp_cmdshell
+*/
+CREATE PROCEDURE disable_xp_cmdshell AS
+BEGIN
+	EXEC sp_configure 'xp_cmdshell', 0
+	reconfigure
+	exec sp_configure 'show advanced options', 0
+	reconfigure
+	RETURN
+END
 go
-EXEC sp_configure 'xp_cmdshell', 1
-go
-reconfigure
+
+
+
+
+/*
+	PROCEDURE QUE ENCAPSULA OS PROCEDIMENTOS DE:
+		- HABILITAR O xp_cmdshell,
+		- EXUCUTAR A FUNÇÃO exportToFile E
+		- DESABILITAR O xp_cmdshell
+
+	@param: datasource (composto pelo nome do banco + nome do eschema + nome da tabela
+	@param: file_target(é o caminho (diretório + nome do arquivo) para onde os dados devem ser exportados
+	@param: uma string com opções de configuração do BCP. O padrão é ' -T -c -t;'
+
+	EXEMPLOS DE USO:
+		- exec dbo.runExportToFile @datasource='db_Escola.dbo.tbl_alunos', @file_target='C:/BCP/tbl_alunos25.txt', @options= default
+		- exec dbo.runExportToFile @datasource='db_Escola.dbo.tbl_alunos', @file_target='C:/BCP/tbl_alunos26.txt', @options=' -T -c -t"|"'
+*/
+CREATE PROCEDURE runExportToFile(@datasource text, @file_target text, @options text = ' -T -c -t;') AS
+BEGIN
+	exec dbo.enable_xp_cmdshell
+	select dbo.exportToFile(@datasource, @file_target, @options)
+	exec dbo.disable_xp_cmdshell	
+	RETURN
+END
 go
